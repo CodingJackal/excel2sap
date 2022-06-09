3d0291919b5b2adfa0712600a77b24561ee321aa
CodingJackal <just@fakemail.com>
2022-06-10 00:39:47 +0200

    Nullbetrag Filter implementiert

--- excel2sap.js
+++ excel2sap.js
@@ -1,6 +1,19 @@
 'use strict'
+// Prueft an welcher Stelle der Betrag steht.
+function findBetragIndex(objects) {
+	const EUR = "EUR";
+	let betragIndex = null;
 
-function createOutput(objects) {	
+	for (const KEY in objects[0]) {
+		if (objects[0][KEY] == EUR) {
+			betragIndex = KEY - 1;
+		}
+	}
+
+	return betragIndex;
+}
+
+function createOutput(objects, betragIndex) {	
 // 	let table = document.createElement("table");		
 // 	document.getElementById("outputBox").appendChild(table);
 
@@ -34,11 +47,20 @@ function createOutput(objects) {
 
 	let outputArea = document.createElement("textarea");
 	let superString = "";
+	let removedRows = 0; // Ungenutzt
+	let summe = 0;
+	
   outputArea.setAttribute("cols", "100");
   outputArea.setAttribute("rows", "20");
 	outputArea.setAttribute("id", "outputArea");
 	
 	for (let i = 0; i < objects.length; i++) {
+		summe += parseFloat(objects[i][betragIndex].replace(",","."));
+
+		if (objects[i][betragIndex] == "0,00") {
+			removedRows++;
+			continue;
+		}
 		Object.values(objects[i]).forEach(val => 
 							superString = superString.concat(val + "\t"));
 		superString = superString.slice(0, -1) + "\n";
@@ -47,6 +69,7 @@ function createOutput(objects) {
 	outputArea.value = superString;	
 	document.getElementById("outputBox").appendChild(outputArea);
 	outputArea.select();
+	alert(summe);
 }
 
 function clickButton() {
@@ -54,23 +77,22 @@ function clickButton() {
 	const text = textArea.value;
 	let textArray = text.split("\n");
 	let objArray = [];
+	let betragIndex = null;
 
 	for (let i = 0; i < textArray.length; i++) {
-		//let tmpArray = textArray[i].split("\t"); <- ändern
-		let tmpArray = textArray[i].split("\t");
-		let tmpObj = { kostenstelleAlt: tmpArray[0],
-					   iaAlt: tmpArray[1],
-					   kostenart: tmpArray[2],
-					   betrag: tmpArray[3],
-					   eur: tmpArray[4],
-					   kostenstelleNeu: tmpArray[5]
-					 }
+		let tmpRowArray = textArray[i].split("\t");
+		let tmpObj = {};
+
+		for (let i = 0; i < tmpRowArray.length; i++) {
+			tmpObj[i] = tmpRowArray[i];
+		}
 		objArray.push(tmpObj);
 	}
+	betragIndex = findBetragIndex(objArray);
 
-	createOutput(objArray);
+	document.getElementById("controlBox").innerHTML = parseFloat("2,5".replace(",",".")) * 10;
+	createOutput(objArray, betragIndex);
 }
-
 
 const control = document.getElementById('controlBox');
 const buttons = container.querySelectorAll('input');
